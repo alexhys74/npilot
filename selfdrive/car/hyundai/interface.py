62150d7729365d62f5a258c1ee9d6bfb12166afe
alexhys74 <alexhys74@gmail.com>
2022-07-03 18:42:08 +0900

    torque값 수정

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -288,9 +288,9 @@ class CarInterface(CarInterfaceBase):
 
     if ret.lateralTuning.which() == 'torque':
       #selfdrive/car/torque_data/params.yaml 참조해서 값 입력 https://codebeautify.org/jsonviewer/y220b1623
-      torque_lat_accel_factor = 3.0787027729757632 #LAT_ACCEL_FACTOR		
-      torque_friction = 0.1207019341823945 #FRICTION
-      ret.maxLateralAccel = 2.6173437483495565 #MAX_LAT_ACCEL_MEASURED		
+      torque_lat_accel_factor = 2.5250855675875634 #LAT_ACCEL_FACTOR		
+      torque_friction = 0.13391574986922777 #FRICTION
+      ret.maxLateralAccel = 1.8303582523301922 #MAX_LAT_ACCEL_MEASURED		
       #토크
       set_torque_tune(ret.lateralTuning, torque_lat_accel_factor, torque_friction)
 
